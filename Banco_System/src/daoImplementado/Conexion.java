3df22aa1a52087598ca892c6f1bde75fd32b8568
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2023-12-06 11:25:43 -0300

    Capa conexion en desarrollo

---
+++ Banco_System/src/daoImplementado/Conexion.java
@@ -0,0 +1,5 @@
+package daoImplementado;
+
+public class Conexion {
+
+}
---
+++ Banco_System/src/servlets/ServletUsuario.java
@@ -0,0 +1,56 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletUsuario
+ */
+@WebServlet("/ServletUsuario")
+public class ServletUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if("btnListar" != null) {
+			
+			System.out.println("llego");
+			
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/listarUsuarios.jsp");
+			rd.forward(request, response);
+		}
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
